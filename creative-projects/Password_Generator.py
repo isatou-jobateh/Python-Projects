ae0020278636333bab463b9f9db81dde5aff087c
Isatou Jobateh <q6ktpw8xhc@privaterelay.appleid.com>
2026-07-13 19:18:53 +0000

    Update Password_Generator

--- creative-projects/Password_Generator.py
+++ creative-projects/Password_Generator.py
@@ -13,19 +13,8 @@ def password_generator():
     symbols = string.punctuation
 
     for i in range(user):
-        first_password = random.choice(lowercase_letters + uppercase_letters + numbers + symbols)
-        second_password = random.choice(lowercase_letters + uppercase_letters + numbers + symbols)
-        third_password = random.choice(lowercase_letters + uppercase_letters + numbers + symbols)
-        fourth_password = random.choice(lowercase_letters + uppercase_letters + numbers + symbols)
-        fifth_password = random.choice(lowercase_letters + uppercase_letters + numbers + symbols)
-        sixth_password = random.choice(lowercase_letters + uppercase_letters + numbers + symbols)
-
-    password += first_password
-    password += second_password
-    password += third_password 
-    password += fourth_password
-    password += fifth_password
-    password += sixth_password
+        charater = random.choice(lowercase_letters + uppercase_letters + numbers + symbols)
+        password = password + charater
 
     print(f"Your password is : {password}")
 
